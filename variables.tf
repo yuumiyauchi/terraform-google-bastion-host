1a0bb6e9f585ee00439e326fb163de4d5fc1244d
yuumiyauchi <165982845+yuumiyauchi@users.noreply.github.com>
2024-06-26 14:59:49 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -86,12 +86,14 @@ variable "network" {
   type = string
 
   description = "Self link for the network on which the Bastion should live"
+　default     = "projects/my-project/global/networks/default"
 }
 
 variable "project" {
   type = string
 
   description = "The project ID to deploy to"
+　default     = "my-default-project-id"
 }
 
 variable "host_project" {
@@ -158,6 +160,7 @@ variable "subnet" {
   type = string
 
   description = "Self link for the subnet on which the Bastion should live. Can be private when using IAP"
+　default     = "projects/my-project/regions/us-central1/subnetworks/default-subnet"
 }
 
 variable "zone" {
